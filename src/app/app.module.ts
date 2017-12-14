09dafe471c9fce734c1cf096e8ac191f4a5202af
konrad <konrad.jahn.84@gmail.com>
2017-12-14 18:01:56 +0100

    tableCell rework done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,9 @@ import { MembersService } from './members.service';
 import { TableComponent } from './table/table.component';
 import { NavComponent } from './nav/nav.component';
 import { InfoBoxComponent } from './info-box/info-box.component';
-// import { PercentPipe } from './pipes/percent.pipe';
-// import { PartyIconPipe } from './pipes/party-icon.pipe';
-// import { YearsPipe } from './pipes/years.pipe';
-// import { LinkPipe } from './pipes/link.pipe';
-// import { PercentVotesToolTipPipe } from './pipes/percent-votes-tool-tip.pipe';
-// import { NThPipe } from './pipes/n-th.pipe';
+import { PercentPipe } from './pipes/percent.pipe';
+import { YearsPipe } from './pipes/years.pipe';
+import { NThPipe } from './pipes/n-th.pipe';
 import { TableCellComponent } from './table-cell/table-cell.component';
 
 @NgModule({
@@ -21,12 +18,9 @@ import { TableCellComponent } from './table-cell/table-cell.component';
     TableComponent,
     NavComponent,
     InfoBoxComponent,
-    // PercentPipe,
-    // PartyIconPipe,
-    // YearsPipe,
-    // LinkPipe,
-    // PercentVotesToolTipPipe,
-    // NThPipe,
+    PercentPipe,
+    YearsPipe,
+    NThPipe,
     TableCellComponent,
   ],
   imports: [
--- src/app/table-cell/table-cell.component.ts
+++ src/app/table-cell/table-cell.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NgSwitch } from '@angular/common';
+
 
 @Component({
   selector: 'app-table-cell',
@@ -10,10 +12,38 @@ export class TableCellComponent implements OnInit {
   @Input() val: string;
   @Input() format: string;
   @Input() formatPars: string[];
+  party: string;
+  url: string;
+
+
+
+  setVals(): void {
+    switch (this.format) {
+      case 'party':
+        switch (this.val) {
+          case 'R':
+            this.party = 'Republican';
+            this.url = 'assets/images/republican.png';
+            break;
+          case 'D':
+            this.party = 'Democrat';
+            this.url = 'assets/images/democrat.png';
+            break;
+          case 'R':
+            this.party = 'Independent';
+            this.url = 'assets/images/independent.png';
+            break;
+        }
+        break;
+
+
+    }
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.setVals();
   }
 
 }
